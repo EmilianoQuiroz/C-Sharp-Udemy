477f745b67d0a2bd4a0190159f4d56202fa277d6
Emiliano Quiroz <santiagoquiroz.dev@gmail.com>
2022-11-19 01:05:06 -0300

    Sintaxis basica en C#

---
+++ 00-MyFirstAppSol/MyFirstApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyFirstApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Cada sentencia o instruccion que termino en C# va con punto y coma
+            //Creamos una variable para almacenar el string
+            string message = string.Empty;
+            //Pedimos al usuario que digite un texto cualquiera
+            Console.WriteLine("Proporciona un mensaje por favor: ");
+            //Guardamos el texto en la variable de tipo string
+            message = Console.ReadLine();
+            //Mostramos en pantalla el texto concatenado
+            Console.WriteLine("Tu cadena es: " + message);
+            Console.Read();
+        }
+    }
+}
