acc86c47f4b7d05ea6d961005321dc9a9e33918f
Emiliano Quiroz <santiagoquiroz.dev@gmail.com>
2022-11-20 01:21:16 -0300

    Calculadora en C#

---
+++ 04-Calculadora/04-Calculadora/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int operation;
+            int firstData;
+            int secondData;
+
+            Console.WriteLine("Bienvenido a la calculadora");
+            Console.WriteLine("1. Suma");
+            Console.WriteLine("2. Resta");
+            operation = GetIntegerDataFromUser("Proporciona la operacion que desea ejecutar: ");
+
+            switch (operation)
+            {
+                case 1:
+                    firstData = GetIntegerDataFromUser("Proporciona el primer operando, debe ser un entero ");
+                    secondData = GetIntegerDataFromUser("Proporciona el segundo operando, debe ser un entero ");
+                    Console.WriteLine($"El resultado de {firstData} + {secondData} = {firstData + secondData}");
+                    break;
+                case 2:
+                    firstData = GetIntegerDataFromUser("Proporciona el primer operando, debe ser un entero ");
+                    secondData = GetIntegerDataFromUser("Proporciona el segundo operando, debe ser un entero ");
+                    Console.WriteLine($"El resultado de {firstData} - {secondData} = {firstData - secondData}");
+                    break;
+                default:
+                    Console.WriteLine("La opcion seleccionada no existe");
+                    break;
+            }
+
+            Console.WriteLine("Precione cualquier tecla para salir");
+            Console.Read();
+        }
+
+        private static int GetIntegerDataFromUser(string message)
+        {
+            string userData;
+            int data = 0;
+            bool inDataValid = false;
+            while (!inDataValid)
+            { 
+                Console.WriteLine(message);
+                userData = Console.ReadLine();
+                if (!int.TryParse(userData, out data))
+                {
+                    Console.WriteLine("El dato proporcionado no es valido. Vuelve a intentarlo");
+                }
+                else
+                {
+                    inDataValid = true;
+                }
+            }
+            return data;
+        }
+    }
+}
