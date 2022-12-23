2b359140befdf46115d5f7e0873d9a6299dd73bf
Emiliano Quiroz <santiagoquiroz.dev@gmail.com>
2022-12-23 03:05:08 -0300

    Practica Listas en C#

--- 07-PracticaListas/07-PracticaListas/Program.cs
+++ 07-PracticaListas/07-PracticaListas/Program.cs
@@ -8,6 +8,45 @@ namespace Lists
         { 
             //Creando instancias de una lista
             Console.ForegroundColor = ConsoleColor.Red;
+            List<int> list1 = new List<int>();
+            list1.Add(10);
+            list1.Add(20);
+            list1.Add(30);
+            list1.Add(40);
+            list1.Add(50);
+            list1.Add(60);
+            list1.Add(70);
+            ShowListContain(list1);
+            Console.ReadKey();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            if (list1.Contains(50))
+            {
+                list1.Remove(50);
+                Console.WriteLine("Se removio el 50");
+            }
+            ShowListContain(list1);
+            Console.ReadKey();
+
+            //Creacion de una lista en una sola linea de codigo 
+            Console.ForegroundColor = ConsoleColor.Blue;
+            List<int> list2 = new List<int>() { 80, 90, 100, 110};
+            ShowListContain(list2);
+            Console.ReadKey();
+
+            //Para concatenar dos listas podemos usar la siguiente funcion
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            list2.InsertRange(0, list1);/*En la posicion 0 de nuestra list2 incertaremos nuestra list1*/
+            ShowListContain(list2);/*Ahora mostramos la list2 ya modificada*/
+            Console.ReadKey();
+        }
+
+        private static void ShowListContain(List<int> list)
+        {
+            foreach (var item in list)
+            { 
+                Console.WriteLine(item);
+            }
         }
     }
 }
